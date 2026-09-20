ab0ba273c701b2b4e44ce4a3d6dfaa4e90da22e2
Afnan <kariox364@gmail.com>
2026-09-20 19:49:34 +0530

    Remove empty extras.css and its import

--- web/src/planbound/main.jsx
+++ web/src/planbound/main.jsx
@@ -3,7 +3,6 @@ import { HashRouter } from "react-router-dom";
 import PlanBoundApp from "./PlanBoundApp";
 import { TasksProvider } from "./state/tasks";
 import "./planbound.css";
-import "./extras.css";
 
 // Not wrapped in StrictMode on purpose: the original engine owns WebGL
 // contexts and global listeners and is started/disposed once per page load.
